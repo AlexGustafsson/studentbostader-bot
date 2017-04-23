e26cd4b289b1677c0c60a65045234e7aa617de64
Alex Gustafsson <AlexGustafsson@users.noreply.github.com>
2017-04-23 21:05:59 +0200

    Fixed Date.now() typo

--- utilities/daemon.js
+++ utilities/daemon.js
@@ -16,33 +16,32 @@ const END_UPTIME = 18;
 
 function startFetchingAccommodations(botpress) {
   // Only update during active hours
-  const hour = new Date(Date.now).getHours();
-  if (hour < START_UPTIME || hour > END_UPTIME)
-    return;
+  const hour = new Date(Date.now()).getHours();
+  if (hour >= START_UPTIME && hour <= END_UPTIME) {
+    console.log('Starting fetching and broadcasting cycle');
 
-  console.log('Starting fetching and broadcasting cycle');
+    // Get current accommodations from server
+    api.fetchAccommodations()
+    .then(accommodations => {
+      console.log('Retrieved accommodations');
+      // Compare to already stored accommodations
+      diffAccommodations(botpress, accommodations)
+      .then(newAccommodations => {
+        console.log(`Diffed accommodations and found ${newAccommodations.length} new ones (vs ${accommodations.length} in total)`);
 
-  // Get current accommodations from server
-  api.fetchAccommodations()
-  .then(accommodations => {
-    console.log('Retrieved accommodations');
-    // Compare to already stored accommodations
-    diffAccommodations(botpress, accommodations)
-    .then(newAccommodations => {
-      console.log(`Diffed accommodations and found ${newAccommodations.length} new ones (vs ${accommodations.length} in total)`);
+        // No new accommodations, don't proceed
+        if (newAccommodations.length === 0)
+          return;
 
-      // No new accommodations, don't proceed
-      if (newAccommodations.length === 0)
-        return;
+        notifySubscribers(botpress, newAccommodations);
 
-      notifySubscribers(botpress, newAccommodations);
-
-      console.log('Storing accommodations');
-      storeAccommodations(botpress, accommodations);
+        console.log('Storing accommodations');
+        storeAccommodations(botpress, accommodations);
+      });
+    }).catch(error => {
+      console.error('Error when automatically updating accommodations', error);
     });
-  }).catch(error => {
-    console.error('Error when automatically updating accommodations', error);
-  });
+  }
 
   // Run every ten minutes
   setTimeout(startFetchingAccommodations.bind(this, botpress), 10 * 60 * 1000);
